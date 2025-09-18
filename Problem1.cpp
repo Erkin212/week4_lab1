b09c4b491ec9e4a72837f1d9f12e025019f52a49
Muhammadaziz <mirzakarimovmuhammadaziz24@gmail.com>
2025-09-18 09:40:36 +0500

    first commit

---
+++ Problem1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    double number;
+    std::cout << "Enter a number: " <<std::endl;
+    std::cin >> number;
+    if (number>=0) {
+        std::cout << "Positive" << std::endl;
+    }
+    else {
+        std::cout << "Negative" << std::endl;
+    }
+
+    return 0;
+}
